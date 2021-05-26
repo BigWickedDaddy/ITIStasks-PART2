71988acb0b3534fb64b29ae92f0a7b72893f9e2b
Oleg S <olegsolovyanenko@gmail.com>
2021-05-26 12:53:15 +0300

    Коммит

---
+++ Todo/Todo/Todo/Views/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Todo.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        async void OnSaveClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TodoItemPage
+            {
+                BindingContext = new TodoItem()
+            });
+        }
+    }
+}
